090690131dd0671dbe8b464290da41726f63e999
Artem Smirnov <artem@smirnov.page>
2022-03-15 12:18:15 +0200

    RawDocumentData docs

--- sdk/src/main/java/com/example/sdk/data/model/id/RawDocumentData.kt
+++ sdk/src/main/java/com/example/sdk/data/model/id/RawDocumentData.kt
@@ -5,14 +5,28 @@ import android.os.Parcelable
 import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Raw document text data consisting of text blocks
+ *
+ * @property blocks List of text blocks
+ *
+ * @see Block
+ * @see Line
+ */
 @Parcelize
 data class RawDocumentData(
     val blocks: List<Block>
 ) : Parcelable {
 
+    /**
+     * Document data is valid if we have at least one block
+     */
     @IgnoredOnParcel
     val isValid = blocks.isNotEmpty()
 
+    /**
+     * Concatenates all lines with a \n
+     */
     override fun toString(): String {
         return blocks
             .asSequence()
@@ -21,6 +35,15 @@ data class RawDocumentData(
             .joinToString("\n")
     }
 
+    /**
+     * Text block
+     *
+     * @property lines List of text lines
+     * @property language Detected language of the block in ISO 639-1
+     * @property boundingBox Bounding box of the block on the original image. Can be null
+     *
+     * @see Line
+     */
     @Parcelize
     data class Block(
         val lines: List<Line>,
@@ -28,10 +51,20 @@ data class RawDocumentData(
         val boundingBox: Rect?
     ) : Parcelable {
 
+        /**
+         * Block is valid if contains at least one line
+         */
         @IgnoredOnParcel
         val isValid = lines.isNotEmpty()
     }
 
+    /**
+     * Text line
+     *
+     * @property text Detected text, never blank
+     * @property language Detected language of the line in ISO 639-1
+     * @property boundingBox Bounding box of the line on the original image. Can be null
+     */
     @Parcelize
     data class Line(
         val text: String,
@@ -39,6 +72,9 @@ data class RawDocumentData(
         val boundingBox: Rect?
     ) : Parcelable {
 
+        /**
+         * Line is valid if it is not blank
+         */
         @IgnoredOnParcel
         val isValid = text.isNotBlank()
     }
