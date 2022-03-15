ae5854f951069211dc8d7d7851b5f4419fb49539
Artem Smirnov <artem@smirnov.page>
2022-03-15 12:11:48 +0200

    FaceResult docs

--- sdk/src/main/java/com/example/sdk/data/model/face/FaceResult.kt
+++ sdk/src/main/java/com/example/sdk/data/model/face/FaceResult.kt
@@ -4,8 +4,19 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import java.io.File
 
+/**
+ * Result of the face detection
+ *
+ * @see FaceData
+ */
 sealed class FaceResult : Parcelable {
 
+    /**
+     * Face detection was successful: found exactly one face, head is not rotated too much, eyes are open
+     *
+     * @property photoFile [File] containing the photo
+     * @property faceData [FaceData] containing the face detection information
+     */
     @Parcelize
     data class Success(
         val photoFile: File,
@@ -13,14 +24,22 @@ sealed class FaceResult : Parcelable {
     ) : FaceResult()
 
     /**
+     * Face detection failed, probable reasons: no faces, multiple faces, eyes closed, head rotated too much
+     *
+     * @property text Error message
+     *
      * TODO: Returning just a text is not the best solution.
-     * We should better return some kind of error class, like enum class FailureType: MissingFace|MultipleFaces|etc
+     *
+     * TODO: We should better return some kind of error class, like enum class FailureType: MissingFace|MultipleFaces|etc
      */
     @Parcelize
     data class Failure(
         val text: String
     ) : FaceResult()
 
+    /**
+     * Face detection was cancelled
+     */
     @Parcelize
     object Cancelled : FaceResult()
 }
