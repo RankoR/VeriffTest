39efaf8f54d84ed58a22edf20b3e8605f046b099
Artem Smirnov <artem@smirnov.page>
2022-03-15 13:12:48 +0200

    Permissions docs

--- core/src/main/java/com/example/core/domain/ArePermissionsGranted.kt
+++ core/src/main/java/com/example/core/domain/ArePermissionsGranted.kt
@@ -4,10 +4,26 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
 
+/**
+ * Check if given permissions are granted
+ */
 interface ArePermissionsGranted {
+
+    /**
+     * Check if all permissions are granted
+     *
+     * @param permissions Permissions to check
+     *
+     * @return True if all permissions are granted
+     */
     fun exec(vararg permissions: String): Boolean
 }
 
+/**
+ * Implementation of permissions check
+ *
+ * @param context [Context] to use (any context)
+ */
 internal class ArePermissionsGrantedImpl(
     private val context: Context
 ) : ArePermissionsGranted {
