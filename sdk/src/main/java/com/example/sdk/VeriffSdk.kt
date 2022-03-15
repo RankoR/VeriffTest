7b2ce2bf66126f417ec33174bf5efd942f0a294b
Artem Smirnov <artem@smirnov.page>
2022-03-15 02:17:48 +0200

    VeriffSdk.kt docs

--- sdk/src/main/java/com/example/sdk/VeriffSdk.kt
+++ sdk/src/main/java/com/example/sdk/VeriffSdk.kt
@@ -6,6 +6,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.FragmentActivity
+import com.example.sdk.VeriffSdk.launchFaceRecognition
+import com.example.sdk.VeriffSdk.launchIdRecognition
+import com.example.sdk.VeriffSdk.registerActivity
 import com.example.sdk.data.model.face.FaceResult
 import com.example.sdk.data.model.id.TextDocumentResult
 import com.example.sdk.di.DiHolder
@@ -14,11 +17,14 @@ import com.example.sdk.presentation.face.FaceRecognitionContract
 import com.example.sdk.presentation.id.IdRecognitionActivity
 import com.example.sdk.presentation.id.IdRecognitionContract
 import timber.log.Timber
-import java.lang.ref.WeakReference
 
+/**
+ * Main Veriff SDK object
+ *
+ * Call [registerActivity] on [Activity] creation, then
+ * call [launchIdRecognition] to launch the ID recognition or [launchFaceRecognition] for face recognition
+ */
 object VeriffSdk {
-
-    private var activity: WeakReference<FragmentActivity>? = null
 
     private var idRecognitionLauncher: ActivityResultLauncher<Unit>? = null
     private var faceRecognitionLauncher: ActivityResultLauncher<Unit>? = null
@@ -26,9 +32,25 @@ object VeriffSdk {
     var isInitialized = false
         private set
 
+    /**
+     * Invoked on ID recognition result
+     *
+     * @see TextDocumentResult
+     */
     var onTextDocumentResult: ((result: TextDocumentResult) -> Unit) = {}
+
+    /**
+     * Invoked on face detection result
+     *
+     * @see FaceResult
+     */
     var onFaceResult: ((result: FaceResult) -> Unit) = {}
 
+    /**
+     * Called once to initialize the required components, such as DI and logging
+     *
+     * @param application [Application] instance for initialization
+     */
     @Synchronized
     private fun initialize(application: Application) {
         initializeDi(application)
@@ -37,12 +59,17 @@ object VeriffSdk {
         isInitialized = true
     }
 
+    /**
+     * Call this method in every activity, where you plan to launch the verification flows.
+     *
+     * If you'll not call it, flows won't be launched
+     *
+     * @param activity [FragmentActivity] to register
+     */
     fun registerActivity(activity: FragmentActivity) {
         if (!isInitialized) {
             initialize(activity.application)
         }
-
-        this.activity = WeakReference(activity)
 
         idRecognitionLauncher = activity.registerForActivityResult(IdRecognitionContract()) { textDocumentResult ->
             Timber.d("Document result: $textDocumentResult")
@@ -57,32 +84,66 @@ object VeriffSdk {
         }
     }
 
+    /**
+     * Launch the ID recognition flow.
+     *
+     * After the flow is complete (or cancelled) you'll get [onTextDocumentResult] callback invoked
+     *
+     * @see onTextDocumentResult
+     */
     fun launchIdRecognition() {
         idRecognitionLauncher?.launch(Unit)
     }
 
+    /**
+     * Launch the face recognition flow.
+     *
+     * After the flow is complete (or cancelled) you'll get [onFaceResult] callback invoked
+     *
+     * @see onFaceResult
+     */
     fun launchFaceRecognition() {
         faceRecognitionLauncher?.launch(Unit)
     }
 
+    /**
+     * Initialize the dagger DI component
+     *
+     * @param application [Application] instance to use for initialization
+     */
     private fun initializeDi(application: Application) {
         DiHolder.initialize(application)
     }
 
+    /**
+     * Initialize logging
+     */
     private fun initializeLogging() {
         Timber.plant(DiHolder.sdkComponent.loggingTree)
 
         Timber.d("Initialized logging")
     }
 
+    /**
+     * Create [Intent] for launching the ID recognition activity
+     *
+     * @param context [Context] to use (must be an activity context, otherwise throws [IllegalArgumentException])
+     * @return [Intent] for launching the [IdRecognitionActivity]
+     */
     internal fun createIdRecognitionIntent(context: Context): Intent {
-        require(context is Activity) { "You must pass an Activity context" } // TODO: Document it
+        require(context is Activity) { "You must pass an Activity context" }
 
         return Intent(context, IdRecognitionActivity::class.java)
     }
 
+    /**
+     * Create [Intent] for launching the face recognition activity
+     *
+     * @param context [Context] to use (must be an activity context, otherwise throws [IllegalArgumentException])
+     * @return [Intent] for launching the [FaceRecognitionActivity]
+     */
     internal fun createFaceRecognitionIntent(context: Context): Intent {
-        require(context is Activity) { "You must pass an Activity context" } // TODO: Document it
+        require(context is Activity) { "You must pass an Activity context" }
 
         return Intent(context, FaceRecognitionActivity::class.java)
     }
